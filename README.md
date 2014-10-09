a1b1b750b0d0dc51db049d44b1109ead7577e03b
Petri Coetzee <petri@semantica.co.za>
2014-10-09 15:20:08 +0200

    bash file and readme update

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ mtr-boilerplate-advanced
 ========================
 
 A bash script to generate an advanced file layout for meteorjs
+
+Just a little bash script I've been using to automate generating a boilerplate for meteor. Very noobish-alpha, feel free to use and edit.
+
+To use, create meteor project with create, then remove all files.
+Then copy bash file into project dir and run.
+
+This will also add some packages for jade template support, autoforms and a few other.
---
+++ meteor_project_boilerplate_advanced.sh
@@ -0,0 +1,11 @@
+mkdir client && mkdir client/compatibility && mkdir client/templates && mkdir client/controllers && mkdir server && mkdir server/initializations && mkdir lib && mkdir packages &&mkdir public && mkdir public/images && mkdir tests
+touch client/main.js && touch main.jade && touch client/subscribtions.js && touch client/routes.js
+touch server/publications.js && touch server/enironment.js && touch server/methods.js && touch lib/schemas.js && touch lib/collections.js
+
+meteor add mquandalle:jade
+meteor add mizzao:bootstrap-3
+meteor add aldeed:collection2
+meteor add aldeed:autoform
+meteor add splendido:accounts-templates-bootstrap
+meteor remove insecure
+meteor remove autopublish
